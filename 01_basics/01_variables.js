61c18a98a264cf8570d8af3476b2515977c1187d
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-01-23 20:00:23 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID=1425
+let accountemail="krishnarathod7353@gamil.com"
+var acountPasword="121563"
+accountcity="Bengaluru"
+ let accountstate;
+
+console.log(accountID)
+
+let name="Krishna"
+ accountemail="kalu.7353@gmail.com"
+//  accountID=545
+
+
+
+console.table([accountID,accountemail,acountPasword,accountcity,accountstate,name])
