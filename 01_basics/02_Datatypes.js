4358e08211f3e0c20a76df606cc329f7e93a8fb3
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-01-25 09:20:15 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat new version of javascriptcode
+
+console.log("Krishna")
+
+
+
+
+
+let name="Krishna"
+let age=18
+let isLoggedin = false
+let state;
+
+
+// premative 
+//number =>2 to power 52
+// bigint 
+// string
+// boolean
+//null-> standalone value
+// undifefined =>
+// symbol=>unique
+
+// non premative
+// object
+// array 
+//function 
+// date
+
+
+console.log(typeof state)
