0c575b532e17d0c60a00159d36bbf581a00d02ca
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-01-28 21:09:25 +0000

    ..

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,76 @@
+let score="33"
+// console.log(typeof score)
+// console.log(typeof(score))
+let numberInumber= Number(score)
+console.log(numberInumber); 
+
+
+
+// 33->33
+//"33abc"->NaN
+//true->1;false->0
+
+
+let isLoggedIn=1
+ let booleanIsLoggedIn=Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn)
+
+ // 0->false;1->True 
+ // exmaple
+ // 1524-true
+ //-0->false
+ //-1->true
+ 
+
+
+
+
+ let someNumber=7855
+ let stringNumber=String(someNumber)
+ console.log(typeof(stringNumber))
+
+
+
+
+
+
+
+
+ //****************************operater*********************************
+
+
+
+ let value=3
+ let negvalue=-value
+//  console.log(negvalue)
+
+
+//  console.log(2+2)
+//  console.log(2-2)
+//  console.log(2*2)
+//  console.log(2**2)
+//  console.log(2/2)
+//  console.log(2%2)
+
+let str1="HII"
+let str2=" KRISHNA"
+// console.log(str1 + str2)
+
+
+//  console.log("1"+2);
+//  console.log(1+"2")
+//  console.log(1+1+"6")
+//  console.log((2+2)*2/4);
+
+
+
+ let gameCounter=3;
+
+ gameCounter++;
+//  console.log(gameCounter)
+ 
+ let x=4;
+ let y=x++;
+//  console.log(y)
+
+ 
