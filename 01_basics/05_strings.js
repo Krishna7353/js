02c0d31806123f836fafa1f3af268d57080e0337
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-01-30 20:30:43 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,76 @@
+const name='Krishna '
+const repo=50
+// console.log(name+ repo +' value' )
+// console.log(`hello my name is  ${name} and repo count ${repo}`);
+// console.log(`hello myself ${name} and repo count ${repo}`);
+
+ const gameName = new String('krishna')
+// console.log(gameName[0]);
+// console.log(gameName[8])
+// console.log(gameName.__proto__);
+
+
+
+// console.log(gameName.length);
+// console.log(gameName.padStart());
+// console.log(gameName.padEnd());
+
+
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+
+
+
+console.log(gameName.charAt(6));
+console.log(gameName.indexOf('a'));
+// console.log(gameName.startsWith('k'));
+// console.log(gameName.endsWith('a'));
+
+
+// console.log(gameName.search('a'));
+ console.log(gameName.substring(0,7))
+
+
+
+ const newString = gameName.substring(0,5)
+//  console.log(newString)
+
+
+let l=gameName.substring(0,9)
+// console.log(l);
+
+
+
+const anotherString=gameName.slice(-7,3)
+// console.log(anotherString);
+
+
+
+const  NewString = "     krishna   Rathod   "
+// console.log(NewString);
+// console.log(NewString.trimEnd())
+
+// console.log(NewString.trim());
+
+
+
+
+const url="krishnaRathod7353@gamil.com"
+// console.log(url.replace('7353','1234'));
+// console.log(url.replace('krishna','kalu'));
+
+// console.log(url.includes('krishna'));
+
+
+const kalu='kishna-rathod-com'
+console.log(kalu.split("-"))
+// console.log(kalu.bold('krishna'));
+
+
+
+
+
+
+
+
