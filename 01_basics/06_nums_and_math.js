858ef08e239f36e6561ce28e46fdc0cf5efebc75
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-02-01 19:32:23 +0000

    math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,58 @@
+const number=100
+// console.log(number);
+// console.log(number.typeof);
+
+
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.typeof);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const orthernumber=23.8966
+// console.log(orthernumber.toPrecision(2));
+
+
+const numbers=10000
+// console.log(numbers.toLocaleString('en-IN'));
+
+
+
+//++++++++++++++++++++++++++++++++++++++++MATH++++++++++++++++++++++++++
+// console.log(Math);
+
+// console.log(Math.abs(-6));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.4));
+// console.log(Math.min(1,4,8,9));
+// console.log(Math.max(1,4,8,9));
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10 )+1);
+// console.log (Math.floor(Math.random()*10) +1);
+
+
+
+
+// const min=10;
+// const max=20;
+// console.log (Math.floor(Math.random()*(min-max +1))+min);
+
+
+
+
+
+const min=15;
+const max=18;
+console.log(Math.floor(Math.random()*(max-min + 1))+max)
+
+
+
+
+
+
+
