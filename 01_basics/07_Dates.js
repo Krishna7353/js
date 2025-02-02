4429441bb268bff31f73f85ec1a960f9711b7f74
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-02-02 20:52:09 +0000

    dates

---
+++ 01_basics/07_Dates.js
@@ -0,0 +1,84 @@
+let mydate=new Date()
+// console.log(mydate);
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(mydate.toString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toUTCString());
+
+
+
+
+// let myCreatedDate= new Date(2023, 0, 23)
+// let myCreatedDate= new Date(2023,0,23,5,4)
+// let myCreatedDate= new Date("2023-01-14")
+let myCreatedDate= new Date("12-11-2003")
+
+
+
+console.log(myCreatedDate.toLocaleString());
+
+
+
+
+let timeStamp=Date.now()
+console.log(timeStamp)
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000))
+
+
+
+ let newDate=new Date;
+ console.log(newDate)
+ console.log(newDate.getMonth()+1);
+ 
+//  console.log(newDate.getDay())
+
+console.log(`${newDate.getMonth()+1} and the time is`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+newDate.toLocaleString('default',{
+    weekday
+})
+console.log(newDate);
+
+
+
+
+
+
+
+
+
