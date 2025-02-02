7d2a5c96f12db28610dc5f39866d775c4b076ffe
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-02-02 20:53:30 +0000

    array method

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,56 @@
+const myARR=[0,1,2,3,4,5]
+const myHeros=["Kalu","shaktiman",'giggle']
+
+
+const newArr=new Array(1,2,3,4,)
+// console.log(myARR[1]);
+
+
+//Array methods
+
+// myARR.push(6)
+// myARR.pop()
+// myARR.pop()
+
+
+// myARR.unshift(6)
+// myARR.shift()
+// myARR.shift()
+
+
+// console.log(myARR.includes(9));
+// console.log(myARR.indexOf(7));
+
+const newarr=myARR.join()
+// console.log(myARR);
+// console.log(newarr);
+
+
+
+//slice,splice
+// console.log('A',myARR);
+// const mya1=myARR.slice(1,3)
+// console.log(mya1);
+
+
+
+// console.log('B',myARR);
+// const mya2=myARR.splice(1,3)
+
+// console.log('c',myARR);
+// console.log(mya2);
+
+
+
+console.log(myARR);
+
+
+
+
+
+
+
+
+
+
+
