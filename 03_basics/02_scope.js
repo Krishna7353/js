62cd7140d36fe4ef0305c5e1aab4ccffb0d56d8d
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-02-05 09:47:40 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,71 @@
+
+var c= 30;
+
+
+if(true){
+const a= 10;
+let b= 20;
+// console.log("inner",a)
+}
+
+
+
+
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+
+
+
+
+
+
+
+
+
+
+function one () 
+{
+ const username="Krishna"  
+ function two() {
+   const website=" Youtube"
+    // console.log(username,website);
+    // console.log(website);
+    
+} 
+// console.log(website);
+
+two()
+}
+one()
+
+
+
+
+if(true){
+    const username="Krishna"
+    if(username==="Krishna"){
+        const website1=" Youtube"
+        console.log(username+website1);
+    }
+//    console.log(websit e1)
+}
+
+
+// console.log(username);
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++impo++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(addone(4))
+function addone(num1){
+    return num1+1
+
+}
+// console.log(addone(4))
+
+console.log(addtwo(4))
+
+const  addtwo=function(num1){
+    return num1+2
+}
