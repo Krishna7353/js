8fcf07e8f26b4f120d0706b8f4a8950c58af3207
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-02-08 16:50:04 +0000

    control flow code

---
+++ 04_controlflow/one.js
@@ -0,0 +1,61 @@
+// const userLoggedIn=true
+
+if(2==='2'){
+    console.log("Executed")
+}
+// console.log("Not equal");
+
+
+
+// const temparature=40
+// if(temparature === 41){
+//     console.log("tempature less than 50");
+// }
+// else{
+//     console.log("temparture greater than 50");
+// }
+// console.log("Executed");
+
+// <,>,<=,>=,!=,===,==.!==
+
+
+const score=200;
+if(score>100){
+   const power="Fly"
+    // console.log(`user power ${power}`);
+}
+// console.log(`user power ${power}`);
+
+const balance=1000;
+// if (balance>500) console.log("TEST1"),console.log("TEST2");
+
+
+
+// if (balance<500) {
+//     console.log("Balance is less than 500"); 
+// } else if (balance<750) {
+//     console.log("Balance is less than 750");
+// }else if (balance<900) {
+//     console.log("Balance is less than 900");
+// }
+// else{
+//     console.log("Balnace less than  1200");
+    
+    
+// }
+
+
+
+const userLoggedIn=true
+const debtcard=true
+const loggedINFromGoogle=true
+const loggedInFromEmail=false
+if(userLoggedIn  && debtcard && 2==3){
+    console.log("Allow to buy a course");
+}
+if(loggedINFromGoogle||loggedInFromEmail){
+    console.log("user logged in");  
+}
+
+
+
