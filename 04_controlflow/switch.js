1535407848e9a5a01d190daff7ec20975a9a20d0
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2025-02-08 16:50:39 +0000

    control flow code

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,29 @@
+     
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month='Jan'
+switch(month){
+    case 'Jan':{
+        console.log("January");     
+    }break;
+    case 'feb':{
+        console.log("feb");    
+    }break;
+    case 'mar':{
+        console.log("march");
+    }break;
+    case 'apr':{
+    }break;
+    default:{
+     console.log("not valid!!")
+    }
+}
